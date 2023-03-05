e617f71c0ad1fe2ec20ba1256b31b38efedefee1
kkmarron <aoyujeong@naver.com>
2023-03-05 11:58:31 +0900

    ninth commit

--- nav_bar/main.js
+++ nav_bar/main.js
@@ -6,3 +6,22 @@ toogleBtn.addEventListener('click', () => {
     menu.classList.toggle('active');
     icons.classList.toggle('active');
 });
+
+const id = document.getElementById('id-field');
+const pswd = document.getElementById('pswd-field');
+const login = document.getElementById('login-btn');
+
+login.addEventListener('click', () => {
+    if(id.value == "") {
+        alert('아이디를 입력해 주세요!');
+    }
+
+    else if(pswd.value == "") {
+        alert("비밀번호를 입력해 주세요!");
+    }
+
+    else {
+        alert("로그인 성공!");
+        window.location.href="Home.html";
+    }
+});
